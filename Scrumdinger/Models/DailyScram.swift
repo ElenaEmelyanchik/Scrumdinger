6a756b23f15958b09758c85e1e654581e395e954
Alena Vasilenka <emelyanchik.elena@gmail.com>
2024-05-27 12:44:47 +0200

    SwiftUI test project

---
+++ Scrumdinger/Models/DailyScram.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScram.swift
+//  Scrumdinger
+//
+//  Created by Elena Vasilenko on 27/05/2024.
+//
+
+import Foundation
---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by Elena Vasilenko on 27/05/2024.
+//
+
+import Foundation
