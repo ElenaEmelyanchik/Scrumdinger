cfcd57ab5dbe97801a5814a59c9eb05eb9cb7de5
Alena Vasilenka <emelyanchik.elena@gmail.com>
2024-05-27 12:52:40 +0200

    init commit 2

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct CardView: View {
+    var scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Text(scrum.title).font(.headline)
+            Spacer()
+            HStack{
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lenghtInMinutes)", systemImage: "clock")
+            }
+        }
     }
 }
 
-#Preview {
-    CardView()
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
 }
