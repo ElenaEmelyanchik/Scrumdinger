9834cf60bbb46ad3b30e907d9a4bd720ad1bb646
Alena Vasilenka <emelyanchik.elena@gmail.com>
2024-05-27 11:12:35 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Elena Vasilenko on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
