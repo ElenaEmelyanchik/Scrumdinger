53f5c653315df523fd6fc3d21017fe75958d09b9
Alena Vasilenka <emelyanchik.elena@gmail.com>
2024-05-29 12:26:51 +0200

    add list and details view

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Elena Vasilenko on 29/05/2024.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,8 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Elena Vasilenko on 29/05/2024.
+//
+
+import Foundation
